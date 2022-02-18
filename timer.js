65fdb7aef05987438aa59b932145c50b02827f01
Catrina Alexandru <ovidiualex.catrina@gmail.com>
2022-02-18 19:29:59 +0200

    other commit

--- timer.js
+++ timer.js
@@ -1,22 +1,27 @@
 let container=document.querySelector(".container");
+
 let min=5;
 let timerDuration=min*60;
 let timer=document.getElementById("timer");
 
 
-let btnStart=document.querySelector(".start");
-btnStart.addEventListener("click",startTimer);
+/*let btnStart=document.querySelector(".start");
+btnStart.addEventListener("click",startTimer);*/
 
 
 
 function startTimer(){
-  
+    
     let mins=Math.floor(timerDuration/60);
     let secs=timerDuration%60;
+        
     secs= secs<10 ? '0'+ secs:secs;
     
     timer.innerHTML=`${mins}:${secs}`;
+       
     timerDuration--;
+    timerDuration = timerDuration < 0 ? 0 : timerDuration; 
+   
 
     
 }
